8bd26c416f537362dca939db777b00c27b1920e5
Alex <afloresescarcega@utexas.edu>
2021-10-11 17:34:29 -0500

    Working multiplication and division with commandline prompt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from itertools import combinations_with_replacement
+from itertools import chain
+from unittest import result
+import sys
 
 class Solver:
     def __init__(self):
@@ -8,28 +11,64 @@ class Solver:
         if int(start) == int(goal):
             return True
         return False
+
+    def add(self, first, second):
+        return first + second
+
+    def multiply(self, first, second):
+        return first * second
+
+    def divide(self, first, second):
+        return first / second
+
+    def run_functions(self, start, list_to_check):
+        result = start
+        for i in range(0, len(list_to_check), 2):
+            op = list_to_check[i]
+            operand = list_to_check[i + 1]
+            if op == '+':
+                result = self.add(result, operand)
+            elif op == '-':
+                result = self.add(result, -operand)
+            elif op == '*':
+                result = self.multiply(result, operand)
+            elif op == '/' and operand != 0:
+                result = self.divide(result, operand)
+        return result
     
     def solve(self, start, goal, operators=[], max_moves=10):
-        for roll in combinations_with_replacement(operators, max_moves):
-            if self.check(sum([int(x) for x in roll]), goal, roll):
+        for roll in combinations_with_replacement(operators*max_moves, max_moves):
+            roll = tuple(x for x in roll if x != '/0')
+            list_to_check = list(chain.from_iterable((x[0], int(x[1::])) for x in roll))
+            if self.check(self.run_functions(start, list_to_check), goal, roll):
                 return roll
         return None
             
         
 # create a main function to run the program
 def main():
-    # create a solver object
     solver = Solver()
-
-    # get the start and goal
-    start = input("Enter the start: ")
-    goal = input("Enter the goal: ")
-
-    # get the operators
-    operators = input("Enter the operators: ")
-
-    # solve the problem
-    solver.solve(start, goal, operators)
+    # if has args, use them as start and goal
+    if len(sys.argv) > 4:
+        start = int(sys.argv[1])
+        goal = int(sys.argv[2])
+        max_moves = int(sys.argv[3])
+        operators = sys.argv[4].split(' ')
+        print(operators)
+    
+        # run solver
+        result = solver.solve(start, goal, operators, max_moves=max_moves)
+        # print result
+        if result is not None:
+            print(result)
+        else:
+            print('No solution found')
+    else:
+        start = int(input("Enter the start: "))
+        goal = int(input("Enter the goal: "))
+        operators = input("Enter the operators: ")
+        max_moves = int(input("Enter the max amoun of movest: "))
+        print(solver.solve(start, goal, operators.split(" "), max_moves=max_moves))
 
 # run the main function
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -7,18 +7,46 @@ class TestMain(unittest.TestCase):
         self.solver = Solver()
 
     def test_simpleAddition(self):
-        self.assertEqual(self.solver.solve(0, 5, ["2", "3"], 2), ("2", "3"))
+        self.assertEqual(self.solver.solve(0, 5, ["+2", "+3"], 2), ("+2", "+3"))
+
+    def test_simpleAddition_startingAtOne(self):
+        self.assertEqual(self.solver.solve(1, 6, ["+2", "+3"], 2), ("+2", "+3"))
+
+    def test_simpleAddition_trickWithZero(self):
+        self.assertEqual(self.solver.solve(0, 5, ["+0", "+2", "+3"], 2), ("+2", "+3"))
+
+    def test_simpleSubtraction(self):
+        self.assertEqual(self.solver.solve(0, 3, ["-1", "+4"], 2), ("-1", "+4"))
 
     def test_simpleAddition2(self):
-        self.assertEqual(self.solver.solve(0, 2, ["1", "1"], 2), ("1", "1"))
+        self.assertEqual(self.solver.solve(0, 2, ["+1", "+1"], 2), ("+1", "+1"))
     
     def test_simpleCannotSolve(self):
-        self.assertEqual(self.solver.solve(0, 5, ["2"], 1), None)
+        self.assertEqual(self.solver.solve(0, 5, ["+2"], 1), None)
+
+    def test_simpleCannotSolve_notEnoughMoves(self):
+        self.assertEqual(self.solver.solve(0, 8, ["+2", "+3"], 1), None)
 
     def test_mediumSimpleAddition(self):
-        self.assertEqual(self.solver.solve(0, 8, ["2", "3"], 3), ("2", "3", "3"))
-        
+        self.assertEqual(self.solver.solve(0, 8, ["+2", "+3"], 3), ("+2", "+3", "+3"))
 
+    def test_simpleMultiplication(self):
+        self.assertEqual(self.solver.solve(0, 6, ["+2", "*3"], 2), ("+2", "*3"))
+
+    def test_simpleMultiplication_trickWithZero(self):
+        self.assertEqual(self.solver.solve(0, 6, ["*0", "+2", "*3"], 2), ("+2", "*3"))
+
+    def test_simpleDivide(self):
+        self.assertEqual(self.solver.solve(10, 2, ["/5"], 1), ("/5",))
+
+    def test_simpleDivide_divideWithZero(self):
+        self.assertEqual(self.solver.solve(10, 2, ["/0","/5"], 2), ("/5",))
+
+    def test_simpleDivide_divideZero(self):
+        self.assertEqual(self.solver.solve(1, 1, ["/1","-1"], 2), ("/1", "/1"))
+
+    def test_needsRepition(self):
+        self.assertEqual(self.solver.solve(0, 20, ["*4","+4"], 3), ('+4', '*4', '+4'))
 
 if __name__ == '__main__':
     unittest.main()
